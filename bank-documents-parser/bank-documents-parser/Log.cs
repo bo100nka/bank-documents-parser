c371abdd7fa1f26485ba136686fd3e4b66bff56e
martin_holkovic <dev.virgil@gmail.com>
2024-01-01 21:23:36 +0100

    first version

---
+++ bank-documents-parser/bank-documents-parser/Log.cs
@@ -0,0 +1,23 @@
+﻿namespace bank_documents_parser
+{
+    internal static class Log
+    {
+        internal static bool DebugMode { get; set; }
+
+        internal static void Info(string message)
+        {
+            Console.WriteLine("INFO:\t" + message);
+        }
+
+        internal static void Error(Exception exception)
+        {
+            Console.WriteLine("ERROR:\t" + exception.ToString());
+        }
+
+        internal static void Error(Exception exception, string message)
+        {
+            var stack = DebugMode ? $"\n{exception.StackTrace}" : default;
+            Console.WriteLine($"ERROR:\t + {exception} - {message}{stack}");
+        }
+    }
+}
