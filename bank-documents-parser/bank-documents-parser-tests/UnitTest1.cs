4e126c614ff56a0c5770eecb369f52c04b71d46b
martin_holkovic <dev.virgil@gmail.com>
2024-01-01 21:28:01 +0100

    add empty xunit test project

---
+++ bank-documents-parser/bank-documents-parser-tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace bank_documents_parser_tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
