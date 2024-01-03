75434d22cf00f9b9939313e99f822fa5008bf689
martin_holkovic <dev.virgil@gmail.com>
2024-01-03 18:47:38 +0100

    minor refactoring of TB parser

--- bank-documents-parser/bank-documents-parser/TatraBankaStatementParser.cs
+++ bank-documents-parser/bank-documents-parser/TatraBankaStatementParser.cs
@@ -65,70 +65,82 @@ namespace bank_documents_parser
             try
             {
                 var lines = text.Split(Environment.NewLine);
-                Log.Info($"Parsing bank statement records from {text?.Length} characters and {lines?.Length} lines...");
+                
+                Log.Info($"Parsing bank statement records from {origin} with {lines?.Length} lines...");
+                
                 var filteredText = FilterLines(lines);
-                var payments = new List<Payment>();
-                var match = Regex.Match(filteredText, RowPattern_Payment, RegexOptions.IgnoreCase | RegexOptions.Multiline);
-
-                while (match.Success)
-                {
-                    var index = match.Index;
-                    var date_process = DateTime.ParseExact(match.Groups["date_process"].Value, "dd.MM.yyyy", System.Globalization.CultureInfo.CurrentCulture);
-                    var date_invoice = !match.Groups["date_invoice"].Success ? default(DateTime?) : DateTime.ParseExact(match.Groups["date_invoice"].Value.TrimStart(), "dd.MM.yyyy", System.Globalization.CultureInfo.CurrentCulture);
-                    var type = match.Groups["type"].Value.ToLowerInvariant();
-                    var account = match.Groups["account"].Value;
-                    var amount = decimal.Parse(match.Groups["amount"].Value);
-                    var payment_id = match.Groups["payment_id"].Value;
-                    var bank_reference = match.Groups["bank_reference"].Value;
-                    var vs = match.Groups["vs"].Value;
-                    var ss = match.Groups["ss"].Value;
-                    var ks = match.Groups["ks"].Value;
-                    var payer_bank = match.Groups["payer_bank"].Value;
-                    var payer_iban = match.Groups["payer_iban"].Value;
-                    var payer_name = match.Groups["payer_name"].Value.Trim();
-                    var detail = match.Groups["detail"].Value;
-                    var payment_type =
-                        type == "tpp" ? PaymentType.Permanent :
-                        type == "platba" ? PaymentType.Manual : 
-                        string.IsNullOrWhiteSpace(type) ? PaymentType.Manual : PaymentType.Unknown;
-
-                    var payment = new Payment
-                    {
-                        Index = index,
-                        DateProcessed = date_process,
-                        DateInvoiced = date_invoice,
-                        PaymentType = payment_type,
-                        Account = account,
-                        Amount = amount,
-                        PaymentId = payment_id,
-                        BankReference = bank_reference,
-                        VariableSymbol = vs,
-                        SpecificSymbol = ss,
-                        ConstantSymbol = ks,
-                        PayerBank = payer_bank,
-                        PayerIban = payer_iban,
-                        PayerName = payer_name,
-                        Detail = detail,
-                        Origin = origin,
-                    };
-
-                    payments.Add(payment);
-
-                    match = match.NextMatch();
-                }
-
+                var payments = ParsePaymentsFromText(origin, filteredText);
                 var result = new ParseResult(text, payments.ToArray());
-                Log.Info($"Parsed {payments?.Count} bank statement records from {text?.Length} characters.");
+                
+                Log.Info($"Parsed {payments?.Count} bank statement records from {origin}.");
+                
                 return result;
             }
             catch (Exception ex)
             {
-                Log.Error(ex, $"Error while parsing bank statement records from {text?.Length} characters");
+                Log.Error(ex, $"Error while parsing bank statement records from {origin}.");
                 return new ParseResult(ex);
             }
         }
 
-        private static string FilterLines(string[] lines)
+        private List<Payment> ParsePaymentsFromText(string? origin, string filteredText)
+        {
+            var payments = new List<Payment>();
+            var match = Regex.Match(filteredText, RowPattern_Payment, RegexOptions.IgnoreCase | RegexOptions.Multiline);
+            var counter = 0;
+
+            while (match.Success)
+            {
+                Log.Info($"Found payment #{++counter} at index {match.Index}: {match.Groups["date_process"]} - {match.Groups["account"].Value} - {match.Groups["amount"].Value}");
+
+                var index = match.Index;
+                var date_process = DateTime.ParseExact(match.Groups["date_process"].Value, "dd.MM.yyyy", System.Globalization.CultureInfo.CurrentCulture);
+                var date_invoice = !match.Groups["date_invoice"].Success ? default(DateTime?) : DateTime.ParseExact(match.Groups["date_invoice"].Value.TrimStart(), "dd.MM.yyyy", System.Globalization.CultureInfo.CurrentCulture);
+                var type = match.Groups["type"].Value.ToLowerInvariant();
+                var account = match.Groups["account"].Value;
+                var amount = decimal.Parse(match.Groups["amount"].Value);
+                var payment_id = match.Groups["payment_id"].Value;
+                var bank_reference = match.Groups["bank_reference"].Value;
+                var vs = match.Groups["vs"].Value;
+                var ss = match.Groups["ss"].Value;
+                var ks = match.Groups["ks"].Value;
+                var payer_bank = match.Groups["payer_bank"].Value;
+                var payer_iban = match.Groups["payer_iban"].Value;
+                var payer_name = match.Groups["payer_name"].Value.Trim();
+                var detail = match.Groups["detail"].Value;
+                var payment_type =
+                    type == "tpp" ? PaymentType.Permanent :
+                    type == "platba" ? PaymentType.Manual :
+                    string.IsNullOrWhiteSpace(type) ? PaymentType.Manual : PaymentType.Unknown;
+
+                var payment = new Payment
+                {
+                    Index = index,
+                    DateProcessed = date_process,
+                    DateInvoiced = date_invoice,
+                    PaymentType = payment_type,
+                    Account = account,
+                    Amount = amount,
+                    PaymentId = payment_id,
+                    BankReference = bank_reference,
+                    VariableSymbol = vs,
+                    SpecificSymbol = ss,
+                    ConstantSymbol = ks,
+                    PayerBank = payer_bank,
+                    PayerIban = payer_iban,
+                    PayerName = payer_name,
+                    Detail = detail,
+                    Origin = origin,
+                };
+
+                payments.Add(payment);
+                match = match.NextMatch();
+            }
+
+            return payments;
+        }
+
+        private string FilterLines(string[] lines)
         {
             var removeLinesThatContain = new[] {
                 "Mena ", 
